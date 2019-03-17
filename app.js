39f8919a50fe9f02b3515a8e0decf7a7ee8c2eab
jeffau13 <jeffau13@gmail.com>
2019-03-16 21:49:15 -0400

    added basic validation and error handling for form

--- app.js
+++ app.js
@@ -12,8 +12,17 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use('/', router);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-//Signup route
-app.post
 
+
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`Server listening on ${port}`));
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,42 @@
 var express = require('express');
+const validator = require('validator');
 var router = express.Router();
+
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
+
+const validate = (errors,req) =>{
+    if (!validator.isEmail(req.body.email)) {
+		errors["message"] = "Please use a valid email.";
+    }
+    if (!validator.isLength(req.body.lastName, {min: 2, max: 30})) {
+		errors["message"] = "Please ensure that your entry has between a minimum of 2 or max 30 characters";
+	}
+}
 
 router.get('/', (req,res,next)=>{
     res.render('index');
 });
 
 router.post('/signup', (req,res,next)=>{
-    
-
+    let errors = {};
+    const { firstName, lastName, email} = req.body;
+    validate(errors,req);
+        if (!isEmpty(errors)){
+            // render_error(errors,req,res,next);
+          return  res.render('failed', {errors});
+        }else{
+           return res.render('success');
+        }
+    })
   
-});
+
+
 
 
 
