468dd6487d66272765999a854df402f35871b3fc
jeffau13 <jeffau13@gmail.com>
2019-03-17 20:03:02 -0400

    implemented feature that resolves entered zip code to state and city

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
+const https = require('https');
 var express = require('express');
 const validator = require('validator');
 var router = express.Router();
 const request = require('request');
-const config = require('./config')
+const config = require('../config');
+const NodeGeocoder = require('node-geocoder');
 
 
 function isEmpty(obj) {
@@ -26,6 +28,10 @@ router.get('/', (req, res, next) => {
     res.render('index');
 });
 
+router.get('/signup', (req,res,next)=>{
+    res.redirect('/');
+})
+
 router.post('/signup', (req, res, next) => {
     let errors = {};
     const { firstName, lastName, email, phone, zipCode, cityName } = req.body;
@@ -34,56 +40,83 @@ router.post('/signup', (req, res, next) => {
         // render_error(errors,req,res,next);
         return res.render('failed', { errors });
     }
+    
 
-    //Construct data to submit:
-
-    const data = {
-        members: [
-            {
-                email_address: email,
-                status: 'subscribed',
-                merge_fields: {
-                    FNAME: firstName,
-                    LNAME: lastName,
-                    PHONE: phone,
-                    CITY: cityName,
-                    ZIP: zipCode
-                }
-            }
-        ]
-    }
-    const postData = JSON.stringify(data);
-    //temp info
-    let dc = config.dc;
-    let listId = config.dc;
-    let key = config.dc;
-
-    const options = {
-        url: `https://${dc}.api.mailchimp.com/3.0/lists/${listId}`,
-        method: 'POST',
-        headers: {
-            Authorization: `auth ${key}`
-        },
-        body: postData
-
+    const geoOptions = {
+        provider: 'google',
+        httpAdapter: 'https',
+        apiKey: config.geoapi
     };
+    const geocoder = NodeGeocoder(geoOptions);
+    
+    let zipCity = '';
+    let state = '';
+ 
+    geocoder.geocode(zipCode, function(err, res) {
+        console.log(res);
+        zipCity = res[0].city;
+        state = res[0].administrativeLevels.level1short;
+        console.log(`ZipCity: ${zipCity}`);
+        sendData();
+    });
+    
+    
+ 
+ 
+    
+    function sendData(){
+    
+    
+    
+        const data = {
+            members: [
+                {
+                    email_address: email,
+                    status: 'subscribed',
+                    merge_fields: {
+                        FNAME: firstName,
+                        LNAME: lastName,
+                        PHONE: phone,
+                        CITY: cityName,
+                        ZIP: zipCode,
+                        STATE: state,
+                        ZIPCITY: zipCity
 
-    request(options, (err, response, body) => {
-        if (err) {
-            return res.render('failed', { err })
-        } else {
-            if (response.statusCode === 200) {
-                res.render('success');
-            } else {
-                err[message] = `Error! statusCode: ${response.statusCode}`
-                return res.render('failed', { err })
-            }
+                    }
+                }
+            ]
+        }
+        const postData = JSON.stringify(data);
+        //temp info
+        let dc = config.dc;
+        let listId = config.listId;
+        let key = config.key;
+
+        const options = {
+            url: `https://${dc}.api.mailchimp.com/3.0/lists/${listId}`,
+            method: 'POST',
+            headers: {
+                Authorization: `auth ${key}`
+            },
+            body: postData
+
         };
 
+        request(options, (err, response, body) => {
+            if (err) {
+                return res.render('failed', { error })
+            } else {
+                if (response.statusCode === 200) {
+                    res.render('success');
+                } else {
+                    res.render('failed');
+                }
+            };
 
-    });
 
+        });
 
+    }
 });
 
 
